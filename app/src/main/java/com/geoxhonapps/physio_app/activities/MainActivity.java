2493ead1bc014d3083c42f0b76b2199a22d000ab
Geoxhon <geoxhon@icloud.com>
2023-05-24 15:36:11 +0300

    Διάφορες βελτιώσεις στο r7

--- app/src/main/java/com/geoxhonapps/physio_app/activities/MainActivity.java
+++ app/src/main/java/com/geoxhonapps/physio_app/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.geoxhonapps.physio_app.activities;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,6 +26,17 @@ public class MainActivity extends ParentActivity {
         Button myButton = findViewById(R.id.loginButton);
         EditText username = findViewById(R.id.username);
         EditText password = findViewById(R.id.password);
+        password.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    StaticFunctionUtilities.attemptLogin(username.getText().toString(), password.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
         myButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 StaticFunctionUtilities.attemptLogin(username.getText().toString(), password.getText().toString());
