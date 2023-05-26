d9617fc81dc120ffdf4f164f1817450cb5d9ac11
ThanasisDoum <thdoumblis@gmail.com>
2023-05-26 21:30:11 +0300

    First Attempt

--- app/src/main/java/com/geoxhonapps/physio_app/activities/AddServiceActivity.java
+++ app/src/main/java/com/geoxhonapps/physio_app/activities/AddServiceActivity.java
@@ -14,20 +14,19 @@ import com.geoxhonapps.physio_app.StaticFunctionUtilities;
 
 import java.util.ArrayList;
 
-public class AddServiceActivity extends ParentActivity implements View.OnClickListener {
+public class AddServiceActivity extends ParentActivity {
 
+    public Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.r2);
-    }
-    @Override
-    public void onClick(View view) {
-        Button btn = findViewById(R.id.r2_save_btn);
+        btn=findViewById(R.id.r2_save_btn);
+        int costasnum=0;
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                int costasnum=0;
                 EditText codetext= findViewById(R.id.r2_code_tv);
                 String c1=codetext.getText().toString();
                 EditText nametext= findViewById(R.id.r2_sname_tv);
@@ -36,14 +35,17 @@ public class AddServiceActivity extends ParentActivity implements View.OnClickLi
                 String c3=desctext.getText().toString();
                 EditText costtext= findViewById(R.id.r2_cost_tv);
                 String c4=costtext.getText().toString();
-                int costasnum=Integer.valueOf(c4);
+                if (!costtext.equals("")){
+                    costasnum=Integer.valueOf(c4);
+                }
 
                 if (c1.length()!=5){
                     Toast.makeText(getApplicationContext(),"Ο κωδικός της υπηρεσίας πρέπει υποχρεωτικά να αποτελείται από 5 χαρακτήρες",Toast.LENGTH_LONG).show();
                 } else if (costasnum<0) {
                     Toast.makeText(getApplicationContext(),"H τιμή της υπηρεσίας δεν μπορεί να είναι αρνητική",Toast.LENGTH_LONG).show();
-                }
-                else{
+                } else if (codetext.equals(" ")|nametext.equals(" ")|desctext.equals(" ")|costtext.equals(" ")) {
+                    Toast.makeText(getApplicationContext(),"Πρέπει να συμπληρωθούν όλα τα πεδία",Toast.LENGTH_LONG).show();
+                } else{
                     if (((AManagerUser) StaticFunctionUtilities.getUser()).createService(c1,c2,c3,costasnum)){
                         ((AManagerUser) StaticFunctionUtilities.getUser()).createService(c1,c2,c3,costasnum);
                         codetext.setText(" ");
@@ -54,11 +56,9 @@ public class AddServiceActivity extends ParentActivity implements View.OnClickLi
                     }
                 }
 
-
             }
         });
     }
-
 
 
 }
