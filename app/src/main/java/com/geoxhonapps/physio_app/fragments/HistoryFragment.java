f0b4cfc96bb3daf8b8cfa8825bf44d96045ca45b
kostas <martinmscofield46@protonmail.com>
2023-06-07 08:48:12 +0300

    Added the r10 xml, previous_order_style.xml that is styling the order rectangle box and a blank HistoryFragment.

--- app/src/main/java/com/geoxhonapps/physio_app/fragments/HistoryFragment.java
+++ app/src/main/java/com/geoxhonapps/physio_app/fragments/HistoryFragment.java
@@ -1,14 +1,32 @@
 package com.geoxhonapps.physio_app.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
+import com.geoxhonapps.physio_app.ContextFlowUtilities;
 import com.geoxhonapps.physio_app.R;
+import com.geoxhonapps.physio_app.RestUtilities.ADoctorUser;
+import com.geoxhonapps.physio_app.RestUtilities.ARecord;
+import com.geoxhonapps.physio_app.StaticFunctionUtilities;
+import com.geoxhonapps.physio_app.activities.AddServiceActivity;
+import com.geoxhonapps.physio_app.activities.HomeActivity;
+import com.geoxhonapps.physio_app.activities.MainActivity;
+import com.geoxhonapps.physio_app.activities.ParentActivity;
+import com.geoxhonapps.physio_app.activities.RecordAppointmentActivity;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * create an instance of this fragment.
@@ -20,6 +38,11 @@ public class HistoryFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.r10, container, false);
         // Inflate the layout for this fragment
+        ArrayList<ARecord> records  = ((ADoctorUser) StaticFunctionUtilities.getUser()).getHistory();
+
+
+
+
         return rootView;
     }
 }
