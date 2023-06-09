4d1058106b6d0e15420d72a7e59776e56dc9afd0
NotGeoxh <geoxhon@gmail.com>
2023-06-09 19:53:33 +0300

    Aesthetic changes on r6

--- app/src/main/java/com/geoxhonapps/physio_app/fragments/CalendarFragment.java
+++ app/src/main/java/com/geoxhonapps/physio_app/fragments/CalendarFragment.java
@@ -1,6 +1,7 @@
 package com.geoxhonapps.physio_app.fragments;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,12 +13,14 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.geoxhonapps.physio_app.ContextFlowUtilities;
 import com.geoxhonapps.physio_app.R;
 import com.geoxhonapps.physio_app.RestUtilities.AAppointment;
 import com.geoxhonapps.physio_app.RestUtilities.ADoctorUser;
+import com.geoxhonapps.physio_app.RestUtilities.EAppointmentStatus;
 import com.geoxhonapps.physio_app.StaticFunctionUtilities;
 
 import java.text.SimpleDateFormat;
@@ -42,6 +45,9 @@ class ListOfDates extends ArrayAdapter<AAppointment>{
         ((TextView)convertView.findViewById(R.id.patientName)).setText(appoint.getAssociatedUser().getDisplayName());
         SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
         ((TextView)convertView.findViewById(R.id.appointmentTime)).setText(timeFormat.format(appoint.getAppointmentDate()));
+        if(appoint.getStatus()==EAppointmentStatus.Pending){
+            ((CardView)convertView.findViewById(R.id.appointmentCard)).setCardBackgroundColor(Color.parseColor("#FFDA56"));
+        }
         return convertView;
     }
 }
@@ -61,13 +67,16 @@ class DateButtonAdapter extends ArrayAdapter<Calendar>{
         SimpleDateFormat dateFormat = new SimpleDateFormat("EEE");
         SimpleDateFormat dayFormat = new SimpleDateFormat("dd");
         Button dateButton = convertView.findViewById(R.id.ButtonDate);
-        dateButton.setText(dateFormat.format(currentDate.getTime()) +" "+ dayFormat.format(currentDate.getTime()));
+        dateButton.setText(dateFormat.format(currentDate.getTime()) +"\n"+ dayFormat.format(currentDate.getTime()));
         AppointmentsList =((ADoctorUser) StaticFunctionUtilities.getUser()).getAppointments(false);
         ArrayList<AAppointment> SpecificDayApp = new ArrayList<>();
         SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
         for(int i=0; i<AppointmentsList.size();i++){
-            if((format.format(AppointmentsList.get(i).getAppointmentDate().getTime())).equals(format.format(currentDate.getTime()))){
-                SpecificDayApp.add(AppointmentsList.get(i));
+            AAppointment temp = AppointmentsList.get(i);
+            if((format.format(temp.getAppointmentDate().getTime())).equals(format.format(currentDate.getTime()))){
+                if(temp.getStatus()!= EAppointmentStatus.Cancelled && temp.getStatus()!= EAppointmentStatus.Completed){
+                    SpecificDayApp.add(AppointmentsList.get(i));
+                }
             }
         }
         for(int i = 0; i<SpecificDayApp.size(); i++){
@@ -130,94 +139,13 @@ public class CalendarFragment extends Fragment {
         LinearLayout dateLayout = rootView.findViewById(R.id.dateButtonsLayout);
         for (int i = 0; i < adapter.getCount(); i++) {
             View view = adapter.getView(i, null, dateLayout, rootView);
+            view.setPadding(0,0,20,0);
+            view.setElevation(20);
             dateLayout.addView(view);
         }
-        // Get references to UI components
-        titleLabel = rootView.findViewById(R.id.MonthText);
-        Button previousButton = rootView.findViewById(R.id.PreviousDate);
-        Button nextButton = rootView.findViewById(R.id.NextDate);
-        // Update the title
-        updateTitle();
-        // Set click listeners for buttons
-        previousButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                date = date - 7;
-                calendar.set(year, month, date);
-                updateTitle();
-            }
-        });
-
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                date = date + 7;
-                calendar.set(year, month, date);
-                updateTitle();
-            }
-        });
         return rootView;
     }
 
-    private void updateTitle() {
-    /*
-        for(int i=0;i<7; i++){
-            calendar.set(year,month,date);
-            Button dateButton;
-            int dayofweek = calendar.get(Calendar.DAY_OF_WEEK);
-
-
-            switch (dayofweek) {
-                case Calendar.SUNDAY:
-
-
-                    dateButton.setText(""+date);
-
-                    break;
-                case Calendar.MONDAY:
-                    dateButton= rootView.findViewById(R.id.Monday);
-                    dateButton.setText(""+date);
-                    break;
-                case Calendar.TUESDAY:
-                    dateButton= rootView.findViewById(R.id.Tuesday);
-                    dateButton.setText(""+date);
-
-                    break;
-                case Calendar.WEDNESDAY:
-                    dateButton= rootView.findViewById(R.id.Wednesday);
-                    dateButton.setText(""+date+"Wednesday");
-
-                    break;
-                case Calendar.THURSDAY:
-                    dateButton= rootView.findViewById(R.id.Thursday);
-                    dateButton.setText(""+date);
-
-                    break;
-                case Calendar.FRIDAY:
-                    dateButton= rootView.findViewById(R.id.Friday);
-                    dateButton.setText(""+date);
-
-                    break;
-                case Calendar.SATURDAY:
-                    dateButton= rootView.findViewById(R.id.Saturday);
-                    dateButton.setText(""+date);
-
-                    break;
-                default:
-                    ContextFlowUtilities.presentAlert("Αποτυχία","Κάτι πηγε λάθος");
-                    break;
-
-            }
-            if(calendar.getActualMaximum(Calendar.DAY_OF_MONTH)>date) {
-                date = date + 1;
-            }else{
-                month= month+1;
-                date=1;
-            }
-
-
-        }*/
-    }
 }
 
 
